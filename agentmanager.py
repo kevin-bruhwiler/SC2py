a3536de042d5068566672a76e7b77b4ec239f807
kevin-bruhwiler <kevin.bruhwiler@gmail.com>
2017-08-22 05:03:15 -0600

    Updated to RL environment

---
+++ agentmanager.py
@@ -0,0 +1,101 @@
+import noise
+import AC
+import memory
+
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.nn as nn
+import torch
+_CUDA = torch.cuda.is_available()
+
+from pysc2.lib import actions
+from pysc2.lib import features
+from pysc2.agents import base_agent
+
+import numpy as np
+import agentmanager
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+_PLAYER_FRIENDLY = 1
+_PLAYER_NEUTRAL = 3  # beacon/minerals
+_PLAYER_HOSTILE = 4
+_MOVE_SCREEN = actions.FUNCTIONS.Move_screen.id
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id
+_NOT_QUEUED = [0]
+_SELECT_ALL = [0]
+
+class Agent(base_agent.BaseAgent):	
+	def __init__(self):
+		super(Agent, self).__init__()
+		self.noise = noise.Noise()
+		self.actor = AC.Actor(256, 256, 2)
+		self.critic = AC.Critic(256, 256, 2)
+		self.memory = memory.Memory(1000000)
+		self.batch_size = 32
+		self.loss = nn.MSELoss()
+		self.actor_optim = optim.Adam(self.actor.parameters(), lr=0.0001)
+		self.critic_optim = optim.Adam(self.critic.parameters(), lr=0.0001)
+		if _CUDA:
+			self.actor.cuda()
+			self.critic.cuda()
+	
+	def setup(self, obs_spec, action_spec):
+		super(Agent, self).setup(obs_spec, action_spec)
+
+	def toVariable(self, x):
+		if _CUDA:
+			return Variable(torch.from_numpy(x).float()).cuda()
+		else:
+			return Variable(torch.from_numpy(x).float())
+
+	def predictReward(self, x, a):
+		return self.critic.forward(x, a)
+		
+	def getReward(self, x, a):
+		y = self.critic.forward(x, a)
+		if self.cuda:
+			return y.cpu().data.numpy().astype(float)
+		return y.data.numpy().astype(float)
+		
+	def predictAction(self, x):
+		return self.actor.forward(x)
+
+	def step(self, obs, x, noise=False):
+		super(Agent, self).step(obs)
+		if _MOVE_SCREEN in obs.observation['available_actions']:
+			y = self.actor.forward(self.toVariable(x))
+			action = y.cpu().data.numpy().astype(float)
+			if noise:
+				action += self.noise.getNoise(action)
+			action = (np.clip(action, 0, 1)*63).tolist()[0]
+			print(action)
+			return actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, action]), action
+		else:
+			return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL]), [-1,-1]
+		
+	def train(self, observation, action, rewards, new_observation):
+		self.memory.add((observation, action, rewards, new_observation))
+		obs, acts, rewards, new_obs = self.memory.sample(self.batch_size)
+		
+		reward_labels = []
+		for i in range(len(new_obs)):
+			if not new_obs[i].any(None):
+				n_o = self.toVariable(np.asarray(new_obs[i])).unsqueeze(0)
+				future_actions = self.predictAction(n_o)
+				future_rewards = self.getReward(n_o, future_actions)
+				reward_labels.append(rewards[i] + 0.9*future_rewards[0])
+			else:
+				reward_labels.append(rewards[i])
+		reward_labels = np.asarray(reward_labels).astype(float)
+				
+		predicted_rewards = self.predictReward(self.toVariable(obs), self.toVariable(acts))
+		reward_loss = self.loss(predicted_rewards, self.toVariable(reward_labels))
+		self.critic_optim.zero_grad()
+		reward_loss.backward()
+		self.critic_optim.step()
+				
+		acts = self.predictAction(self.toVariable(obs))
+		rewards = self.predictReward(self.toVariable(obs), acts)
+		rewards = -torch.mean(rewards)
+		self.actor_optim.zero_grad()
+		rewards.backward()
+		self.actor_optim.step()
--- main.py
+++ main.py
@@ -1,3 +1,69 @@
+from pysc2.env import sc2_env
+from pysc2.lib import features
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+import time, sys
+import agentmanager
+import gflags as flags
+FLAGS = flags.FLAGS
+argv = FLAGS(sys.argv)
+
+def run_loop(agents, env):
+	total_frames = 0
+	start_time = time.time()
+	
+	action_spec = env.action_spec()
+	observation_spec = env.observation_spec()
+	for agent in agents:
+		agent.setup(observation_spec, action_spec)
+		
+	timesteps = env.reset()
+	observation = timesteps[0].observation['screen'][_PLAYER_RELATIVE].reshape((1,64,64))
+	step_count = 0
+	
+	for agent in agents:
+		agent.reset()
+	try:
+		while True:
+			total_frames += 1
+			actions = []
+			targets = []
+			for agent in agents:
+				out = agent.step(timesteps[0],observation.reshape((1,1,64,64)))
+				actions.append(out[0])
+				targets.append(out[1])
+			timesteps = env.step(actions)
+			next_observation = timesteps[0].observation['screen'][_PLAYER_RELATIVE].reshape((1,64,64))
+			reward = timesteps[0].reward
+			done = timesteps[0].last()
+			if done:
+				break
+
+			for agent, action in zip(agents, targets):
+				agent.train(observation, action, reward, next_observation)
+
+			observation = next_observation
+			step_count += 1
+
+	except KeyboardInterrupt:
+		return timesteps
+		
+	elapsed_time = time.time() - start_time
+	print("Took %.3f seconds for %s steps: %.3f fps" % (elapsed_time, total_frames, 
+														total_frames / elapsed_time))
+	return timesteps
 
 if __name__=='__main__':
-	
+	num_episodes = 10000
+	for episode in range(num_episodes):
+		with sc2_env.SC2Env(
+                    "MoveToBeacon",
+                    agent_race='T',
+                    bot_race='P',
+                    difficulty="1",
+                    visualize=True) as env:
+			agent = agentmanager.Agent()
+			run_result = run_loop([agent], env)
+			reward = run_result[0].reward
+			env.close()
+
+		print("%d'th game result :"%episode,reward)
